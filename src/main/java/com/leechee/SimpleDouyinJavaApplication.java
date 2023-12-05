fea64ead0bef10ff79c1a529349441b991cf6512
Leechee403 <Leechee403@Leechee403.com>
2023-12-05 17:27:41 +0800

    :loud_sound: 增加项目运行时日志提醒

--- src/main/java/com/leechee/SimpleDouyinJavaApplication.java
+++ src/main/java/com/leechee/SimpleDouyinJavaApplication.java
@@ -2,12 +2,18 @@ package com.leechee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@Slf4j
 public class SimpleDouyinJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleDouyinJavaApplication.class, args);
+		log.info("server started...");
 	}
 
 }
