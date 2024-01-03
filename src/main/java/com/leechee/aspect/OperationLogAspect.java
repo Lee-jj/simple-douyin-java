a220dc62f29ffd1230ac320a96d6984ee5eb4f25
Leejj <Leejj@Leejj.com>
2024-01-03 18:40:28 +0800

    :construction: 使用AOP记录操作日志到数据库

--- src/main/java/com/leechee/aspect/OperationLogAspect.java
+++ src/main/java/com/leechee/aspect/OperationLogAspect.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import com.leechee.annotation.OperationLog;
 import com.leechee.constant.JwtClaimsConstant;
 import com.leechee.entity.Logs;
+import com.leechee.mapper.LogMapper;
 import com.leechee.properties.JwtProperties;
 import com.leechee.utils.JwtUtil;
 
@@ -29,6 +30,8 @@ public class OperationLogAspect {
     private HttpServletRequest request;
     @Autowired
     private JwtProperties jwtProperties;
+    @Autowired
+    private LogMapper logMapper;
 
     /**
      * 切入点
@@ -51,33 +54,26 @@ public class OperationLogAspect {
             Claims claims = JwtUtil.parseJWT(jwtProperties.getSecretKey(), token);
             userId = claims.get(JwtClaimsConstant.USER_ID).toString();
         } catch (Exception e) {}
-        System.out.printf("userId: %s\t", userId);
 
         // 获取当前操作时间
         LocalDateTime operateTime = LocalDateTime.now();
-        System.out.printf("operateTime: %s\t", operateTime);
 
         // 操作类名
         String className = joinPoint.getTarget().getClass().getName();        
-        System.out.printf("className: %s\t", className);
 
         // 操作方法名
         String methodName = joinPoint.getSignature().getName();
-        System.out.printf("methodName: %s\t", methodName);
 
         // 获取annotation中的description值，即操作描述
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         OperationLog operationLog = signature.getMethod().getAnnotation(OperationLog.class);
         String description = operationLog.description();
-        System.out.printf("description: %s\t", description);
 
         // 获取IP地址
         String ipAddr = request.getRemoteAddr();
-        System.out.printf("ipAddr: %s\t", ipAddr);
 
         // 获取请求URL
         String url = request.getRequestURL().toString();
-        System.out.printf("url: %s\n", url);
 
 
         long begin = System.currentTimeMillis();
@@ -90,20 +86,21 @@ public class OperationLogAspect {
 
         // 记录操作耗时
         Long costTime = end - begin;
-        System.out.printf("costTime: %s\n", costTime);
 
-        // TODO 记录日志
-        // Logs logs = Logs.builder()
-        //        .operation_user_id(userId)
-        //        .method_name(methodName)
-        //        .ip_address(ipAddr)
-        //        .url(url)
-        //        .cost_time(costTime)
-        //        .description(description)
-        //        .operation_date(operateTime)
-        //        .class_name(className)
-        //        .build();
+        // 记录日志
+        Logs logs = Logs.builder()
+               .operation_user_id(userId)
+               .method_name(methodName)
+               .ip_address(ipAddr)
+               .url(url)
+               .cost_time(costTime)
+               .description(description)
+               .operation_date(operateTime)
+               .class_name(className)
+               .build();
+        logMapper.insert(logs);
 
+        log.info("插入操作记录日志: {}", logs);
 
         return result;
     }
---
+++ src/main/java/com/leechee/mapper/LogMapper.java
@@ -0,0 +1,18 @@
+package com.leechee.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import com.leechee.entity.Logs;
+
+@Mapper
+public interface LogMapper {
+    
+    /**
+     * 插入操作日志
+     * @param logs
+     */
+    @Insert("insert into logs (operation_user_id, operation_date, class_name, method_name, ip_address, url, cost_time, description) " 
+    + "values (#{operation_user_id}, #{operation_date}, #{class_name}, #{method_name}, #{ip_address}, #{url}, #{cost_time}, #{description})")
+    void insert(Logs logs);
+}
